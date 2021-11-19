80d99c7024227f8e5855b8d11449953463f827c1
thomasschafer <thomasschafer97@gmail.com>
2021-11-19 21:09:06 +0000

    Refactored chat page and added useLocalStorage hook

--- src/routes/OpenChat.tsx
+++ src/routes/OpenChat.tsx
@@ -3,6 +3,8 @@ import { Link, useParams } from "react-router-dom";
 import io, { Socket } from "socket.io-client";
 import { v4 as uuidv4 } from "uuid";
 
+import { useLocalStorage } from "../hooks/useLocalStorage";
+
 const BACKEND_URL = "localhost:8000";
 
 interface chatMessage {
@@ -22,40 +24,62 @@ const Message = ({ msg, senderId }: { msg: chatMessage; senderId: string }) => (
   </div>
 );
 
-const OpenChat = () => {
-  let params = useParams();
-  const [message, setMessage] = useState("");
-  const [messageThread, setMessageThread] = useState<Array<chatMessage>>([]);
-  const [socket, setSocket] = useState<Socket>();
-  const [senderId, setSenderId] = useState<string>("");
+const MessageThread = ({
+  messageThread,
+  senderId,
+}: {
+  messageThread: Array<chatMessage>;
+  senderId: string;
+}) => (
+  <div className="w-full max-w-screen-lg p-5 pt-20 border-bottom mb-24">
+    {messageThread.length === 0 ? (
+      <>
+        <img
+          className="w-8/12 max-w-sm mx-auto my-10"
+          src={process.env.PUBLIC_URL + "/message-ideas.svg"}
+          alt="Message ideas"
+        />
+        <div className="mx-auto my-10 w-max text-gray-500 text-2xl">Say hi!</div>
+      </>
+    ) : (
+      <></>
+    )}
+    {messageThread.map((msg, idx) => (
+      <Message senderId={senderId} msg={msg} key={`${idx}-${msg.body}`} />
+    ))}
+  </div>
+);
 
-  useEffect(() => {
-    setSenderId(uuidv4());
+const Navbar = ({ chatId }: { chatId: string }) => (
+  <nav className="fixed top-0 bg-indigo-700 h-16 w-full flex flex-col items-center justify-center">
+    <div className="w-full max-w-screen-lg p-5 flex flex-row justify-between items-center">
+      <Link to="/" className="text-white w-5">
+        Home
+      </Link>
+      <h1 className="text-white">{chatId}</h1>
+      <div className="w-5"></div>
+    </div>
+  </nav>
+);
 
-    const newSocket = io(BACKEND_URL);
-    setSocket(newSocket);
-
-    newSocket.on("connect", () => {
-      newSocket.emit("chat-id", params.chatId);
-    });
-
-    newSocket.on("message-received", (msg) => {
-      setMessageThread((thread) => [...thread, msg]);
-      window.scrollTo(0, document.body.scrollHeight);
-    });
-
-    newSocket.on("message-thread", (msg) => {
-      console.log("message-thread", msg);
-    });
-  }, [params]);
-
+const ComposeMessageForm = ({
+  message,
+  setMessage,
+  senderId,
+  socket,
+}: {
+  message: string;
+  setMessage: any;
+  senderId: string;
+  socket: Socket | undefined;
+}) => {
   const updateMessage = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
     setMessage(event.target.value);
   };
 
   const submitMessage = () => {
-    if (socket) {
-      socket.emit("new-message", { senderId: senderId, body: message, chatId: params.chatId });
+    if (socket && message) {
+      socket.emit("new-message", { senderId: senderId, body: message });
       setMessage("");
     }
   };
@@ -73,58 +97,84 @@ const OpenChat = () => {
   };
 
   return (
-    <main className="flex flex-col items-center">
-      <nav className="fixed top-0 bg-indigo-700 h-16 w-full flex flex-col items-center justify-center">
-        <div className="w-full max-w-screen-lg p-5 flex flex-row justify-between items-center">
-          <Link to="/" className="text-white w-5">
-            Home
-          </Link>
-          <h1 className="text-white">{params.chatId}</h1>
-          <div className="w-5"></div>
-        </div>
-      </nav>
+    <div className="fixed bottom-0 w-full bg-gray-100 flex flex-col items-center">
+      <form className="flex flex-row w-full max-w-screen-lg p-5">
+        <textarea
+          className="shadow appearance-none border rounded w-full p-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline resize-none"
+          placeholder="Message"
+          value={message}
+          onChange={updateMessage}
+          onKeyDown={submitOnEnterPress}
+        />
+        <input
+          type="submit"
+          className="my-auto py-2 px-3 cursor-pointer rounded-md ml-2 bg-gray-200 shadow border border-gray-300"
+          value="Send"
+          onClick={handleSubmit}
+        />
+      </form>
+    </div>
+  );
+};
 
+const OpenChat = () => {
+  let params = useParams();
+  const [message, setMessage] = useState("");
+  const [messageThread, setMessageThread] = useState<Array<chatMessage>>([]);
+  const [hasLoaded, setHasLoaded] = useState(false);
+  const [socket, setSocket] = useState<Socket>();
+  const [senderId] = useLocalStorage("chat-senderid", uuidv4());
+  const [userName, setUserName] = useLocalStorage("chat-username", senderId);
+
+  useEffect(() => {
+    const newSocket = io(BACKEND_URL);
+    setSocket(newSocket);
+
+    newSocket.on("connect", () => {
+      newSocket.emit("chat-id", params.chatId);
+    });
+
+    newSocket.on("message-received", (msg) => {
+      setMessageThread((thread) => [...thread, msg]);
+      window.scrollTo(0, document.body.scrollHeight);
+    });
+
+    newSocket.on("message-thread", (msg) => {
+      if (msg) {
+        setMessageThread(msg);
+      }
+      setHasLoaded(true);
+    });
+  }, [params]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      window.scrollTo(0, document.body.scrollHeight);
+    }, 0);
+  }, [messageThread]);
+
+  return (
+    <main className="flex flex-col items-center min-h-screen">
+      <Navbar chatId={params.chatId || ""} />
       {params.chatId && params.chatId.length <= 5 ? (
         <div className="pt-20">Sorry, that chat doesn't exist.</div>
       ) : (
         <>
-          <div className="w-full max-w-screen-lg p-5 pt-20 border-bottom mb-24">
-            {messageThread.length === 0 ? (
-              <>
-                <img
-                  className="w-8/12 max-w-sm mx-auto my-10"
-                  src={process.env.PUBLIC_URL + "/message-ideas.svg"}
-                  alt="Message ideas"
-                />
-                <div className="mx-auto my-10 w-max text-gray-500 text-2xl">Say hi!</div>
-              </>
-            ) : (
-              <></>
-            )}
-            {messageThread.map((msg, idx) => (
-              <Message senderId={senderId} msg={msg} key={`${idx}-${msg.body}`} />
-            ))}
-          </div>
+          {hasLoaded ? (
+            <MessageThread messageThread={messageThread} senderId={senderId} />
+          ) : (
+            <div className="w-full h-screen flex justify-center items-center">Loading...</div>
+          )}
 
-          <div className="fixed bottom-0 w-full bg-gray-100 flex flex-col items-center">
-            <form className="flex flex-row w-full max-w-screen-lg p-5">
-              <textarea
-                className="shadow appearance-none border rounded w-full p-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline resize-none"
-                placeholder="Message"
-                value={message}
-                onChange={updateMessage}
-                onKeyDown={submitOnEnterPress}
-              />
-              <input
-                type="submit"
-                className="my-auto py-2 px-3 cursor-pointer rounded-md ml-2 bg-gray-200 shadow border border-gray-300"
-                value="Send"
-                onClick={handleSubmit}
-              />
-            </form>
-          </div>
+          <ComposeMessageForm
+            message={message}
+            setMessage={setMessage}
+            senderId={senderId}
+            socket={socket}
+          />
         </>
       )}
+      <div className="pb-36">User id: {userName}</div>
     </main>
   );
 };
