72e0b09600d5f6fa1e218a1e6addf8ecfe65f35b
thomasschafer <thomasschafer97@gmail.com>
2021-11-27 15:38:17 +0000

    Updated design of exisiting chat input field

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
 
   return (
     <main className="w-screen h-screen flex justify-center align-middle bg-primaryPurple">
-      <div className="flex flex-col justify-center items-center align-middle py-10 sm:px-10 text-center h-full w-full sm-h-fit-content sm:w-8/12 sm:max-w-screen-sm my-auto rounded-xl bg-white">
+      <div className="flex flex-col justify-center items-center align-middle py-10 sm:px-10 text-center w-11/12 h-fit-content max-w-screen-sm my-auto rounded-xl bg-white">
         <div className="flex-shrink-0">
           <img
             className="h-40 w-40 mb-5 object-cover"
@@ -28,22 +28,24 @@ function App() {
           alt="kobble text"
         />
         <Link
-          className="border-4 border-primaryPurple text-primaryPurple rounded-xl p-3 m-2 w-60 h-16 flex items-center justify-center hover:bg-primaryPurple hover:text-white transition duration-75 font-bold tracking-wide"
+          className="border-4 border-primaryPurple text-primaryPurple rounded-xl p-3 m-2 w-40 sm:w-60 h-16 flex items-center justify-center hover:bg-primaryPurple hover:text-white transition duration-75 font-bold tracking-wide"
           to={`/chat/${uuidv4()}`}
         >
           New chat
         </Link>
         <div
-          className="border-4 border-primaryPurple text-primaryPurple rounded-xl cursor-pointer p-3 m-2 w-60 h-16 flex items-center justify-center hover:bg-primaryPurple hover:text-white transition duration-75 font-bold tracking-wide"
+          className={`border-4 border-primaryPurple text-primaryPurple rounded-xl m-2 w-40 sm:w-60 h-16 flex items-center justify-center hover:bg-primaryPurple hover:text-white transition duration-75 font-bold tracking-wide ${
+            showExistingChatInput ? "bg-primaryPurple" : "cursor-pointer"
+          }`}
           onClick={() => {
             !showExistingChatInput && setShowExistingChatInput(true);
           }}
         >
           {showExistingChatInput ? (
-            <form className="flex flex-row items-center space-x-3">
+            <form className="flex flex-row items-center h-full">
               <input
                 type="text"
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                className="shadow appearance-none rounded-lg border-primaryPurple w-full h-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                 placeholder="Chat ID"
                 value={chatId}
                 onChange={(e) => {
@@ -52,7 +54,7 @@ function App() {
               />
               <input
                 type="submit"
-                className="py-2 px-3 cursor-pointer rounded-md"
+                className="py-2 px-3 h-full cursor-pointer rounded-lg bg-primaryPurple text-white hover:bg-grey font-bold"
                 value="Go"
                 onClick={handleSubmit}
               />
